30056e1a9c3b90e1b03e893798c5711333dc897c
ryo <nakayaryo0716@gmail.com>
2024-03-19 19:15:37 +0900

    fix code to use postgres

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+mod app;
 mod users;
 mod web;
 
 #[tokio::main]
 async fn main() {
-   
+    let app = app::app();
+
+    let listner = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listner, app).await.unwrap();
 }
--- src/users.rs
+++ src/users.rs
@@ -2,7 +2,7 @@ use axum::async_trait;
 use axum_login::{AuthUser, AuthnBackend, UserId};
 use password_auth::verify_password;
 use serde::{Deserialize, Serialize};
-use sqlx::{prelude::FromRow, SqlitePool};
+use sqlx::{prelude::FromRow, PgPool};
 use tokio::task;
 
 #[derive(Debug, thiserror::Error)]
@@ -19,37 +19,34 @@ pub struct User {
     id: i64,
     pub name: String,
     password: String,
-}    
+}
 
 impl AuthUser for User {
     type Id = i64;
     fn id(&self) -> Self::Id {
         self.id
-    }    
+    }
 
     fn session_auth_hash(&self) -> &[u8] {
         &self.password.as_bytes()
-    }    
-}    
+    }
+}
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Credentials {
     pub username: String,
     pub password: String,
     pub next: Option<String>,
-}    
-
+}
 
 #[derive(Debug, Clone)]
 pub struct Backend {
-    pool: SqlitePool,
+    pool: PgPool,
 }
 
 impl Backend {
-    pub fn new(pool: SqlitePool) -> Self {
-        Self {
-            pool,
-        }
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
     }
 }
 
@@ -59,23 +56,26 @@ impl AuthnBackend for Backend {
     type Credentials = Credentials;
     type Error = Error;
 
-    async fn authenticate(&self, creds: Self::Credentials) -> Result<Option<Self::User>, Self::Error> {
+    async fn authenticate(
+        &self,
+        creds: Self::Credentials,
+    ) -> Result<Option<Self::User>, Self::Error> {
         let user = sqlx::query_as("select * from users wher username = ?")
-            .bind(creds.username).
-            fetch_optional(&self.pool)
-            .await?;      
-        
+            .bind(creds.username)
+            .fetch_optional(&self.pool)
+            .await?;
+
         task::spawn_blocking(|| {
             Ok(user.filter(|user: &User| verify_password(creds.password, &user.password).is_ok()))
-        }).await?
-              
+        })
+        .await?
     }
 
     async fn get_user(&self, user_id: &UserId<Self>) -> Result<Option<Self::User>, Self::Error> {
         let user = sqlx::query_as("select * from users where id = ?")
-        .bind(user_id)
-        .fetch_optional(&self.pool)
-        .await?;
+            .bind(user_id)
+            .fetch_optional(&self.pool)
+            .await?;
 
         Ok(user)
     }
