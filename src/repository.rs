a3461fd8cbad731de022ff55303c00633342b5de
ryonakaya <nakayaryo0716@gmail.com>
2024-05-11 22:12:27 +0900

    DB側の基本的な操作(create, reade)をするメソッドの実装

--- src/repository.rs
+++ src/repository.rs
@@ -1,6 +1,5 @@
 use std::{
-    collections::HashMap,
-    sync::{atomic::AtomicI32, Arc, Mutex},
+    collections::HashMap, error::Error, sync::{atomic::AtomicI32, Arc, Mutex}
 };
 
 use serde::{Deserialize, Serialize};
@@ -9,14 +8,14 @@ static USER_ID_PROVIDER: AtomicI32 = AtomicI32::new(0);
 
 // ユーザー
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct User {
+pub struct User {
     user_id: i32,
     user_name: String,
 }
 
 // 新規作成
 #[derive(Debug, Clone, Deserialize)]
-struct CreateUser {
+pub struct CreateUser {
     user_name: String,
 }
 
@@ -33,7 +32,7 @@ impl UserDb {
         }
     }
 
-    pub fn create_user(&self, payload: CreateUser) -> User {
+    pub fn create_user(&self, payload: CreateUser) -> Result<User, Box<dyn Error>> {
         let created_id = USER_ID_PROVIDER.load(std::sync::atomic::Ordering::Relaxed);
         let user = User {
             user_id: created_id,
@@ -46,14 +45,24 @@ impl UserDb {
         }
 
         USER_ID_PROVIDER.store(created_id + 1, std::sync::atomic::Ordering::Relaxed);
-        user
+        Ok(user)
+    }
+
+    pub fn get_user_by_id(&self, id: i32) -> Result<Option<User>, Box<dyn Error>> {
+        let optional_user = None;
+        {
+            let db = self.pool.lock().unwrap();
+            let optional_user = db.get(&id).clone();
+        }
+
+        Ok(optional_user)
     }
 }
 
 // セッション管理するためのDB
 #[derive(Debug, Clone)]
 struct SessionDb {
-    pub pool: Arc<Mutex<HashMap<String, String>>>,
+    pub pool: Arc<Mutex<HashMap<String, User>>>,
 }
 
 impl SessionDb {
@@ -61,6 +70,12 @@ impl SessionDb {
         Self {
             pool: Arc::default(),
         }
+    }
+
+    fn insert_session_info(&self, session_id: String, user_data: User) -> Result<(), Box<dyn Error>>{
+        let mut db = self.pool.lock().unwrap();
+        db.insert(session_id, user_data).unwrap();
+        Ok(())
     }
 }
 
@@ -80,6 +95,7 @@ struct CreateTodo {
 }
 
 //　Todoを格納するためのDB
+#[derive(Debug, Clone)]
 struct TodoDb {
     pool: Arc<Mutex<HashMap<i32, Todo>>>,
 }
@@ -91,7 +107,7 @@ impl TodoDb {
         }
     }
 
-    pub fn create_todo(&self, payload: CreateTodo, user: User) -> Todo {
+    pub fn create_todo(&self, payload: CreateTodo, user: User) -> Result<Todo, Box<dyn Error>> {
         let created_id = TODO_ID_PROVIDER.load(std::sync::atomic::Ordering::Relaxed);
 
         let todo = Todo {
@@ -106,6 +122,15 @@ impl TodoDb {
         }
 
         TODO_ID_PROVIDER.store(created_id + 1, std::sync::atomic::Ordering::Relaxed);
-        todo
+        Ok(todo)
+    }
+
+    pub fn read_todo(&self) -> Result<Vec<Todo>, Box<dyn Error>> {
+        let todo: Vec<Todo>;
+        {
+            let db  = self.pool.lock().unwrap();
+            todo = db.iter().map(|(_id, todo)| todo.clone()).collect();
+        }
+        Ok(todo)
     }
 }
