d16465b221ae65875889d9320959de163074f174
ryonakaya <nakayaryo0716@gmail.com>
2024-05-14 23:53:01 +0900

    FromRefトレイトを使ったState分割で上手くいき、ログインと各ユーザに対するTodoの作成が可能になった

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use axum::extract::FromRef;
 use repository::{SessionDb, TodoDb, UserDb};
 
 mod handler;
@@ -5,12 +6,49 @@ mod repository;
 mod router;
 #[tokio::main]
 async fn main() {
-    let session_store = SessionDb::new();
+    let session_db = SessionDb::new();
     let user_db = UserDb::new();
     let todo_db = TodoDb::new();
-    
-    let app = router::service(user_db, session_store, todo_db);
+
+    let app_state = AppState {
+        user_db,
+        session_db,
+        todo_db,
+    };
+
+    let app = router::service(app_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub user_db: UserDb,
+    pub session_db: SessionDb,
+    pub todo_db: TodoDb,
+}
+
+impl FromRef<AppState> for UserDb {
+    fn from_ref(input: &AppState) -> Self {
+        UserDb {
+            pool: input.user_db.pool.clone(),
+        }
+    }
+}
+
+impl FromRef<AppState> for SessionDb {
+    fn from_ref(input: &AppState) -> Self {
+        SessionDb {
+            pool: input.session_db.pool.clone(),
+        }
+    }
+}
+
+impl FromRef<AppState> for TodoDb {
+    fn from_ref(input: &AppState) -> Self {
+        TodoDb {
+            pool: input.todo_db.pool.clone(),
+        }
+    }
 }
